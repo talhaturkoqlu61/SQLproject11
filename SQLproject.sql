c6a56c6e1b2703ea3f1f501681cfe77f3447d034
Talha Türkoğlu <158575414+talhaturkoqlu61@users.noreply.github.com>
2025-04-16 17:33:57 +0300

    Create SQLproject.sql

---
+++ SQLproject.sql
@@ -0,0 +1,16 @@
+--(SELECT first_name FROM customer)
+--UNION ALL
+--(SELECT first_name FROM actor);
+--(1.QUESTION)
+
+
+--(SELECT first_name FROM customer)
+--INTERSECT 
+--(SELECT first_name FROM actor);
+--(2.QUESTION)
+
+--(SELECT first_name FROM customer)
+--EXCEPT 
+--(SELECT first_name FROM actor);
+--(3.QUESTION)
+
